3efe58da95c9f5aff6cda384a1ba31fb07e54cbf
Vinícius Fonseca <fonsecavinicius117@gmail.com>
2025-12-22 21:26:07 -0300

    first commit

---
+++ src/main/java/team/horizon/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package team.horizon.CadastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+	}
+
+}
