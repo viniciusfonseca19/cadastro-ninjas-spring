f573bb2d2ee5baad3e7f8d2ba70190c364048cde
Vinícius F. <fonsecavinicius117@gmail.com>
2026-01-05 20:34:24 -0300

    Cria uma entidade no banco de dados apartir do NinjaMode

--- src/main/java/team/horizon/CadastroDeNinjas/NinjaModel.java
+++ src/main/java/team/horizon/CadastroDeNinjas/NinjaModel.java
@@ -12,10 +12,10 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String nome;
-    String email;
-    int idade;
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
 
     public NinjaModel() {
 
@@ -50,3 +50,4 @@ public class NinjaModel {
         this.idade = idade;
     }
 }
+//teste
